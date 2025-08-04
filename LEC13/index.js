27dc8f8f3497de4462c88c67615b87f5cf2d35d9
Romshagupta9 <romsha61116@gmail.com>
2025-08-04 15:45:58 +0530

    "LEC-13 added"

---
+++ LEC13/index.js
@@ -0,0 +1,34 @@
+// // create server
+// const express = require("express")
+// const app = express();
+// const fs = require("fs");
+// app.use(express.static(__dirname + "/public"));
+// app.get("/users", (req, res) => {
+//     fs.readFile("users.json", "utf-8", function(err,data) {
+//         if(err) res.send(err)
+//             let allUsers = JSON.parse(data);
+//     })
+// })
+
+// app.listen(2000,()=>{
+//     console.log("server started")
+// })
+
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/users", (req, res) => {
+    fs.readFile("./users.json", "utf-8", function(err, data) {
+        if (err) res.send(err);
+        
+        let allUsers = JSON.parse(data);
+        res.json(allUsers); // Send the users as JSON response
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 2000");
+});
---
+++ LEC13/public/index.js
@@ -0,0 +1,47 @@
+let userContainer = document.querySelector(".userContainer");
+console.log(userContainer);
+
+
+
+function getUserData(URL){
+    fetch(URL)
+    .then((res)=>{
+         console.log(res);
+         return res.json()//to return a json object.., it also returns a prmoise
+    })
+    // .then((data)=>{ // 2 bar .then lgega
+    //      console.log(data); 
+    // })
+
+    .then((data)=>{
+        console.log(data);
+        data.forEach(user=>{
+            displayUsers(user);
+        })
+    })
+    .catch((err)=>{
+         console.log(err);
+    });
+}
+
+//user's data is shown on the page
+function displayUsers(users){
+    let li = document.createElement("li");
+    li.setAttribute("class", "user-item");
+    li.innerHTML = `<div class="userInfo">
+                <h1> ${users.name}</h1>
+                <p>${users.username}</p>
+            </div>
+            <div class="userButton">
+                <button class="user-delete">Delete</button>
+                <button class="user-edit">Edit</button>
+            </div>`
+
+            userContainer.appendChild(li);
+}
+
+// getUserData('https://jsonplaceholder.typicode.com/users')
+getUserData('http://localhost:3000/users'); // Use local server URL
+
+//server bhi apna hoga
+
